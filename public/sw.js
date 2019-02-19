20debda2f6d4ec6f1abf8e7628fdabe675e276a4
Fadli F Zain and Siti Khalishah <pair+fadli.zain+siti.khalishah@wellcode.io>
2019-02-19 17:48:34 +0700

    sw

--- public/sw.js
+++ public/sw.js
@@ -2,12 +2,15 @@ const urlsToCache = [
   '/',
   '/todos'
 ];
-const cacheName = 'pages-cache-v2'
+const cacheName = 'pages-cache-v2';
 
 self.addEventListener('install', function(event) {
+  console.log('Installed hit');
   event.waitUntil(async function() {
+    console.log('Installed hit inside event waituntil');
     const cache = await caches.open(cacheName);
     await cache.addAll(urlsToCache);
+    // console.log('Service worker installed.');
   });
 });
 
@@ -15,27 +18,47 @@ self.addEventListener('activate', function(event) {
   //perform some task
   event.waitUntil(async function() {
     caches.delete('pages-cache-v1');
+
+    // const cache = await caches.open(cacheName);
+    // await cache.addAll(urlsToCache);
+    // console.log('Service worker installed.');
   }());
   console.log('Service worker is activated.');
   //do something
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(async function() {
-    try {
-      return await fetch(event.request)
-        .then( function(response) {
-          event.waitUntil(
-            caches.open(cacheName)
-            .then(cache => {
-              return cache.addAll(urlsToCache);
-            })
-          )
-          return response;
-        });
-    } catch (err) {
-      console.log('cache loaded');
-      return caches.match(event.request);
-    }
-  }());
+
+// Promise.race is no good to us because it rejects if
+// // a promise rejects before fulfilling. Let's make a proper
+// // race function:
+// function promiseAny(promises) {
+  // console.log("promis", promises);
+  // return new Promise((resolve, reject) => {
+    // promises.forEach(p => console.log('Output :' + p));
+    // // make sure promises are all promises
+    // promises = promises.map(p => Promise.resolve(p));
+    // // resolve this promise as soon as one resolves
+    // promises.forEach(p => p.then(resolve));
+    // // reject if all promises reject
+
+    // promises.reduce((a, b) => a.catch(() => b))
+      // .catch(() => reject(Error("All failed")));
+  // });
+// };
+
+
+self.addEventListener('fetchz', (event) => {
+  // try {
+    event.respondWith(
+      promiseAny([
+        caches.match(event.request),
+        fetch(event.request)
+      ])
+    );
+  // } catch(err) {
+    // console.log('Error : ', err);
+  // } finally {
+    // console.log(event.request.url)
+  // };
 });
+
