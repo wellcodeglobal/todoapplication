d5ece43ad1a793b21fa7b4b8735655864c25c133
Fadli F Zain and Siti Khalishah <pair+fadli.zain+siti.khalishah@wellcode.io>
2019-02-19 11:25:32 +0700

    on activate delete cache

--- public/sw.js
+++ public/sw.js
@@ -4,17 +4,29 @@ const urlsToCache = [
 const urlsToCacheLater = [
   '/todos'
 ];
-const cacheName = 'pages-cache-v1'
+const cacheName = 'pages-cache-v2'
 
 self.addEventListener('install', function(event) {
   event.waitUntil(async function() {
     const cache = await caches.open(cacheName);
     cache.addAll(urlsToCacheLater);
     await cache.addAll(urlsToCache);
+  });
 });
 
 self.addEventListener('activate', function(event) {
   //perform some task
+  event.waitUntil(async function() {
+    const cacheNames = await caches.keys();
+    await Promise.all(
+      cacheNames.filter((cacheName) => {
+        // Return true if you want to remove this cache,
+        // but remember that caches are shared across
+        // the whole origin
+        console.log('cache deleted');
+      }).map(cacheName => caches.delete('pages-cache-v1'))
+    );
+  }());
   console.log('Service worker is activated.');
   //do something
 });
